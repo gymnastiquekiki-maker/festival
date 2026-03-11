6e1528fbf46f80be211993971cdb17095a5f1f33
kristyna svobodova <gymnastiquekiki@gmail.com>
2026-03-11 09:04:44 +0100

    Add browser XSLT to JSON transform

---
+++ web/transform.js
@@ -0,0 +1,79 @@
+﻿(function () {
+  var runBtn = document.getElementById("run");
+  var output = document.getElementById("output");
+
+  var transforms = [
+    { name: "info.json", xslt: "xslt/festival-info.xslt", match: "/festival/info" },
+    { name: "venues.json", xslt: "xslt/festival-venues.xslt", match: "/festival/venues" },
+    { name: "performers.json", xslt: "xslt/festival-performers.xslt", match: "/festival/performers" },
+    { name: "events.json", xslt: "xslt/festival-events.xslt", match: "/festival/events" }
+  ];
+
+  function fetchText(url) {
+    return fetch(url, { cache: "no-store" }).then(function (res) {
+      if (!res.ok) {
+        throw new Error("HTTP " + res.status + " for " + url);
+      }
+      return res.text();
+    });
+  }
+
+  function parseXml(text) {
+    var xml = new DOMParser().parseFromString(text, "application/xml");
+    if (xml.querySelector("parsererror")) {
+      throw new Error("XML parse error");
+    }
+    return xml;
+  }
+
+  function transform(xml, xsltText, contextXPath) {
+    var xsltDoc = parseXml(xsltText);
+    var processor = new XSLTProcessor();
+    processor.importStylesheet(xsltDoc);
+
+    var context = xml.evaluate(contextXPath, xml, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+    if (!context) {
+      throw new Error("Missing node for " + contextXPath);
+    }
+
+    var fragment = processor.transformToFragment(context, document);
+    return (fragment.textContent || "").trim();
+  }
+
+  function download(name, text) {
+    var blob = new Blob([text], { type: "application/json;charset=utf-8" });
+    var url = URL.createObjectURL(blob);
+    var link = document.createElement("a");
+    link.href = url;
+    link.download = name;
+    document.body.appendChild(link);
+    link.click();
+    link.remove();
+    URL.revokeObjectURL(url);
+  }
+
+  runBtn.addEventListener("click", function () {
+    output.textContent = "Načítám...";
+
+    Promise.all([fetchText("festival.xml")].concat(transforms.map(function (t) { return fetchText(t.xslt); })))
+      .then(function (texts) {
+        var xml = parseXml(texts[0]);
+        var results = [];
+
+        transforms.forEach(function (t, i) {
+          var xsltText = texts[i + 1];
+          var jsonText = transform(xml, xsltText, t.match);
+          results.push({ name: t.name, json: jsonText });
+        });
+
+        output.textContent = results.map(function (r) { return "// " + r.name + "\n" + r.json; }).join("\n\n");
+
+        results.forEach(function (r) {
+          download(r.name, r.json + "\n");
+        });
+      })
+      .catch(function (err) {
+        output.textContent = "Chyba: " + err.message;
+      });
+  });
+})();
---
+++ web/xslt/festival-events.xslt
@@ -0,0 +1,14 @@
+﻿<?xml version="1.0" encoding="UTF-8"?>
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+  <xsl:output method="text" encoding="UTF-8"/>
+  <xsl:template match="/festival/events">[
+<xsl:for-each select="event">  {
+    "id": "<xsl:value-of select="@id"/>",
+    "venue_id": "<xsl:value-of select="@venue_id"/>",
+    "performer_id": "<xsl:value-of select="@performer_id"/>",
+    "date": "<xsl:value-of select="@date"/>",
+    "time": "<xsl:value-of select="@time"/>"
+  }<xsl:if test="position() != last()">,</xsl:if>
+</xsl:for-each>
+]</xsl:template>
+</xsl:stylesheet>
